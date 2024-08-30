bb4eea256a2fc5eaeec13708754ea1b50cbdf9fe
Paveltr07 <pavel.tamanis@alumno.buap.mx>
2024-08-29 22:15:24 -0600

    pie.inc.php

--- practicas/p04/pie.inc.php
+++ practicas/p04/pie.inc.php
@@ -4,7 +4,7 @@
             #ffcc99;\"> Final de la página PHP Vínculos útiles : <a href=\"php.net\">php.net</a>
             &nbsp; <a href=\"mysql.org\">mysql.org</a></h1>";
             echo "Nombre del archivo ejecutado: ", $_SERVER['PHP_SELF'],"&nbsp;&nbsp; &nbsp;";
-            echo "Nombre del archivo incluido: ", __FILE__ ,"</div>";
+            echo "Nombre del archivo incluido: ", __FILE__ ,"</div>"; 
         ?>
     </body>
 </html>
